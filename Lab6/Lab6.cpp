f3331e0d31b5a33994f4db652884143840575be5
User#None <77580844+issamansur@users.noreply.github.com>
2023-09-17 23:35:03 +0300

    reformat (2/2)

---
+++ Lab6/Lab6.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	// 5. Change values of two variables
+	
+	// solution:
+	
+	// we will use "eax" - 32 bit 
+	// because int - 4 bytes = 32 bit
+
+	// variant 1 (use 2 registers)
+	int one = 16, two = 32;
+
+	__asm {
+		mov eax, one // eax=16
+		mov ebx, two // ebx=32
+		mov one, ebx // one=32
+		mov two, eax // two=16
+	}
+	cout << "one = " << one << endl; // one=32
+	cout << "two = " << two << endl; // two=16
+
+	// variant 2 (use only 1 register)
+	one = 16, two = 32;
+
+	__asm {		
+		mov eax, one // eax=16
+		xchg eax, two // eax=32, two=16
+		mov one, eax // one = 32
+	}
+	cout << "one = " << one << endl; // one=32
+	cout << "two = " << two << endl; // two=16
+}
---
+++ Lab9/Lab9.cpp
@@ -0,0 +1,114 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	// 5. Write program on c++ and ASM (with inserting)
+	// Compare results
+
+	// 4. Given an array of integers: 
+	// 52, 24, 17, -15, 33, 11, 48, 18, -9, 5. 
+	// Based on this array of integers, 
+	// find their arithmetic mean and construct an array 
+	// whose elements equal to the difference between 
+	// the original elements and the resulting average.
+
+
+	// Solutions:
+	int a[10] = { 52, 24, 17, -15, 33, 11, 48, 18, -9, 5 };
+	int size_array = 10;
+
+	cout << "Input array:" << endl;
+	for (int i = 0; i < size_array; i++)
+		cout << a[i] << " ";
+	cout << endl << endl;
+
+
+	// Solution c++
+	int sum = 0;
+	double b1[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+	for (int i = 0; i < size_array; i++)
+		sum += a[i];
+	cout << "Sum: " << sum << endl << endl;
+
+	double average = sum * 1.0 / size_array;
+	cout << "Average: " << average << endl << endl;
+
+	for (int i = 0; i < size_array; i++)
+		b1[i] = abs(a[i] - average);
+
+	cout << "Output array (by c++):" << endl;
+	for (int i = 0; i < size_array; i++)
+		cout << b1[i] << " ";
+	cout << endl << endl;
+
+
+	// Solution asm
+	sum = 0;
+	double b2[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+	// find sum
+	__asm {
+		// set counter for looping
+		mov ecx, size_array
+		mov esi, 0
+		// set variable for sum
+		mov eax, 0
+
+		// START cycle
+	start:
+		mov eax, [a + esi * 4]
+		add sum, eax
+
+		inc esi
+		// END cycle
+
+		// cycle condition
+		loop start // decrement ecx -> ecx = ecx - 1
+	}
+	cout << "Sum: " << sum << endl << endl;
+
+	// find average
+	_asm {
+		// use FPU (Floating Point Unit):
+		// fild = (floating integer load)
+		fild dword ptr[sum] // FPU = [sum, ...]
+		fild dword ptr[size_array] // FPU = [size_array, sum, ...]
+		// fdiv = (floating-point division)
+		fdiv // FPU = [sum/size_array, size_array, sum, ...]
+		// fstp = (floating - point store and pop)
+		fstp qword ptr[average] // average = sum/size_array
+	}
+	cout << "Average: " << average << endl << endl;
+
+	// set array b2
+	__asm {
+		// set counter for looping
+		mov ecx, size_array
+		mov esi, 0
+		// set variable for sum
+		mov eax, 0
+
+		// START cycle
+	start2:
+		fild dword ptr[a + esi * 4]
+		fld qword ptr[average]
+		// fsub = (floating substraction)
+		fsub // FPU = [b2[i]-average, average, b2[i], ...]
+		fabs
+		fstp qword ptr[b2 + esi * 8]
+
+
+		inc esi
+		// END cycle
+
+		// cycle condition
+		loop start2 // decrement ecx -> ecx = ecx - 1
+	}
+
+	cout << "Output array (by asm):" << endl;
+	for (int i = 0; i < size_array; i++)
+		cout << b2[i] << " ";
+	cout << endl;
+}
